923ab46b5084b1833aa00e1b8f7d9b5a20a4221b
Bright Adu Kwarteng snr <adubrightkwarrteng11@gmail.com>
2023-06-16 23:04:53 +0000

    image preview functionality done

--- filehub/main/views.py
+++ filehub/main/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import File
 from django.db import models
+from django.http import HttpResponse
+import requests
+from urllib.parse import urljoin
 
 
 def home(request):
@@ -19,3 +22,17 @@ def search_view(request):
                                 models.Q(file_type__icontains=query))
     context = {'files':files ,'query':query}
     return render(request, 'main/file_search.html', context)
+
+def preview_file(request,file_id):
+    file_obj = File.objects.get(id=file_id)
+    file_url = file_obj.file.url
+    
+    # Get the base url of the application
+    base_url = request.build_absolute_uri('/')[:-1]
+    
+    # join base_url with file_url to get the absolute_file_url
+    absolute_file_url= urljoin(base_url,file_url)
+    # Get the content of the file
+    response = requests.get(absolute_file_url)
+    
+    return HttpResponse(response.content)
