e376ba739272ea6566aae1070f16cc92ed87dd50
Bright Adu Kwarteng snr <adubrightkwarrteng11@gmail.com>
2023-06-22 14:55:38 +0000

    set debug to false for deployment

--- filehub/filehub/settings.py
+++ filehub/filehub/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "accounts",
     "crispy_forms",
     "crispy_bootstrap4",
-    "storages",
+    
 
 ]
 
