1b1adfe6d9802ba3ad725c8bc115c481a7d70df8
Bright Adu Kwarteng snr <adubrightkwarrteng11@gmail.com>
2023-06-26 10:23:48 +0000

    made connection to railway mysql database

--- filehub/filehub/settings.py
+++ filehub/filehub/settings.py
@@ -86,13 +86,25 @@ WSGI_APPLICATION = "filehub.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+# Railway MySQL Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default":
+        {
+            "ENGINE": "django.db.backends.mysql",
+            "URL":"mysql://root:FQU2F53f42vWqC61g1U0@containers-us-west-38.railway.app:5946/railway",
+            "NAME": "railway",
+            "USER": "root",
+            "PASSWORD": "FQU2F53f42vWqC61g1U0",
+            "HOST":"containers-us-west-38.railway.app",
+            "PORT":"5946"
+        }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
